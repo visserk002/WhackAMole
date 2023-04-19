2027eb1af26b47a55eeda994e0858f891461a7f1
Kevin <kevin@ar-producties.nl>
2023-04-19 22:25:58 +0200

    updated the molespawner class as a first version

--- Assets/Scripts/Objects/MoleSpawner.cs
+++ Assets/Scripts/Objects/MoleSpawner.cs
@@ -1,18 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
+using KevinV.WhackAMole.Interfaces;
 using UnityEngine;
 
-public class MoleSpawner : MonoBehaviour
+namespace KevinV.WhackAMole.Objects
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MoleSpawner : MonoBehaviour
     {
-        
-    }
+        public GameObject molePrefab;
+        public GameObject bonusMolePrefab;
+        public GameObject subtractorMolePrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private float spawnInterval = 1f;
+        private float currentSpawnInterval;
+
+        private void Start()
+        {
+            StartSpawning();
+        }
+
+        public void StartSpawning()
+        {
+            currentSpawnInterval = spawnInterval;
+            InvokeRepeating(nameof(SpawnMole), currentSpawnInterval, currentSpawnInterval);
+        }
+
+        private void SpawnMole()
+        {
+            int moleType = Random.Range(0, 3);
+            IMole mole;
+
+            switch (moleType)
+            {
+                case 0:
+                    mole = Instantiate(molePrefab, transform).GetComponent<IMole>();
+                    break;
+                case 1:
+                    mole = Instantiate(bonusMolePrefab, transform).GetComponent<IMole>();
+                    break;
+                case 2:
+                    mole = Instantiate(subtractorMolePrefab, transform).GetComponent<IMole>();
+                    break;
+                default:
+                    mole = Instantiate(molePrefab, transform).GetComponent<IMole>();
+                    break;
+            }
+
+            mole.Spawn(GetRandomMolePosition());
+        }
+
+        private Vector3 GetRandomMolePosition()
+        {
+            return new Vector3(Random.Range(-3f, 3f), 0f, Random.Range(-2f, 2f));
+        }
+
+        public void SetSpawnInterval(float interval)
+        {
+            currentSpawnInterval = interval;
+            CancelInvoke();
+            InvokeRepeating(nameof(SpawnMole), currentSpawnInterval, currentSpawnInterval);
+        }
+
+        public float GetCurrentSpawnInterval()
+        {
+            return currentSpawnInterval;
+        }
     }
 }
